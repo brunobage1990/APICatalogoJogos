dfc68adb8583c99edac4850555c526336e7d61ed
Bruno <brunobf_52@hotmail.com>
2021-11-08 15:06:17 -0300

    Criando Catálogo de jogos

---
+++ APICatalogojogos/APICatalogojogos/Repositorio/IJogoRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogojogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogojogos.Repositorio
+{
+    public interface IJogoRepositorio : IDisposable
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
+    }
+}
---
+++ APICatalogojogos/APICatalogojogos/Services/IJogoService.cs
@@ -0,0 +1,19 @@
+﻿using APICatalogojogos.InputModel;
+using APICatalogojogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogojogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogosInputModel jogo);
+        Task Atualizar(Guid id, JogosInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
